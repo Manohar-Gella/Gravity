83f824c83c379156781f977c3a07f9c9a7a631b3
Manohar-Gella <manohargella272@gmail.com>
2024-01-04 01:49:21 +0530

    ascii_art

---
+++ main.py
@@ -0,0 +1,9 @@
+from pyfiglet import Figlet
+
+# Create a Figlet font object
+custom_font = Figlet()
+
+# Generate ASCII art from a text string using the custom font
+ascii_art = custom_font.renderText("Hello, ASCII Art!")
+print(ascii_art)
+
