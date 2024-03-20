e3e8900b69ad0f88ddc96d32ccbf6f022bfd9a30
Manohar Gella <manohargella272@gmail.com>
2024-03-20 15:53:38 +0530

    math functions py

---
+++ lessons/7math.py
@@ -0,0 +1,24 @@
+import math
+
+value = 33.3
+
+
+print(round(value))
+print(abs(value))
+print(pow(value, 2))
+
+print(math.ceil(value))
+print(math.floor(value))
+print(math.sqrt(value))
+
+
+a = 20 
+b = 30
+c = 40
+
+print(min(a, b, c))
+print(max(a, b, c))
+
+
+
+
