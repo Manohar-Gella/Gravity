2a3ab86f21fb05e18a801dc2e5a0bbb5f7f167ed
Manohar Gella <85400866+Manohar-Gella@users.noreply.github.com>
2024-05-02 02:41:36 +0530

    Delete server directory

--- server/main.py
+++
@@ -1,9 +0,0 @@
-from pyfiglet import Figlet
-
-# Create a Figlet font object
-custom_font = Figlet()
-
-# Generate ASCII art from a text string using the custom font
-ascii_art = custom_font.renderText("Hello, ASCII Art!")
-print(ascii_art)
-# i am manoj
