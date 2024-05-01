2e47d58d0e3a10b591df301727d550603037bfc5
Manohar Gella <manohargella272@gmail.com>
2024-05-02 02:38:26 +0530

    zig-server

---
+++ http.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn init(allocator: *std.mem.Allocator) Http {
+    return Http{
+        // Initialize HTTP client
+    };
+}
+
+pub struct Http {
+    // Define HTTP client methods
+}
